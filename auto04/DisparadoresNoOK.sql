12a7ede2adedcb4fa04db7ce160c4db0abc935d3
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-03-15 23:21:22 -0500

    se creo disparadores NO ok

---
+++ auto04/DisparadoresNoOK.sql
@@ -0,0 +1,12 @@
+--CATEGORIAS--
+
+INSERT INTO ProductCategory(ProductCategoryID,ParentProductCategoryID,Name) VALUES(42,NULL,'Pants'); 
+INSERT INTO ProductCategory(ProductCategoryID,ParentProductCategoryID,Name) VALUES(43,NULL,'Kneepads'); 
+INSERT INTO ProductCategory(ProductCategoryID,ParentProductCategoryID,Name) VALUES(44,NULL,'Heel');
+
+
+--MODELOS--
+
+INSERT INTO ProductModel(ProductModelID,Name,CatalogDescription) VALUES(132,'HL Touring',NULL); 
+INSERT INTO ProductModel(ProductModelID,Name,CatalogDescription) VALUES(133,'LL Road Front',NULL); 
+INSERT INTO ProductModel(ProductModelID,Name,CatalogDescription) VALUES(134,'ML Road Front',NULL);
