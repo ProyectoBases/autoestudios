23a5aaec7fed9e086afd77ba82a1ad77b5749285
AndresFelipeGualdron <gualdronsito@hotmail.com>
2018-03-15 22:31:57 -0500

    corrección de triggers

--- auto04/acciones.sql
+++ auto04/acciones.sql
@@ -1,4 +1,6 @@
+----CATEGORIA----
+--Las categorias se pueden eliminar si no tinen productos asociados--
 ALTER TABLE ProductCategory 
 DROP CONSTRAINT FK_PARENTCID;
-
 ALTER TABLE ProductCategory ADD CONSTRAINT FK_PARENTCID FOREIGN KEY (ParentProductCategoryID) REFERENCES ProductCategory(ProductCategoryID) ON DELETE CASCADE;
+
--- auto04/disparadores.sql
+++ auto04/disparadores.sql
@@ -1,15 +1,44 @@
-----
-CREATE SEQUENCE secuenciaProductCategory
-INCREMENT BY 1
-START WITH 1;
-
-CREATE OR REPLACE TRIGGER consecutivoProductCategory BEFORE INSERT ON ProductCategory
+---CATEGORIAS----
+--el numero se genera automaticamente de forma consecutiva--
+CREATE OR REPLACE TRIGGER AD_CATEGORIA
+BEFORE INSERT ON ProductCategory
 FOR EACH ROW
-DECLARE cont NUMBER := 0;
+DECLARE cont NUMBER;
 BEGIN
-SELECT secuenciaProductCategory.NEXTVAL INTO cont FROM ProductCategory;
-:NEW.ProductCategoryID := cont;
+SELECT COUNT(ProductCategoryID)+1 INTO cont FROM ProductCategory;
+:NEW.ProductCategoryID:=cont;
+END;
+
+CREATE OR REPLACE TRIGGER EL_CATEGORIA
+BEFORE DELETE ON ProductCategory
+FOR EACH ROW
+BEGIN
+IF :NEW.ParentProductCategoryID = 1
+THEN RAISE_APPLICATION_ERROR(-20000, 'NO SE PUEDE ELIMINAR');
+END IF;
 END;
 
 
-DROP TRIGGER consecutivoProductCategory;
+
+
+
+---MODELOS-----
+--los identificadores se generan de forma consecutiva--
+CREATE OR REPLACE TRIGGER ADD_MODELO
+BEFORE INSERT ON ProductModel
+FOR EACH ROW
+DECLARE cont NUMBER;
+BEGIN
+SELECT COUNT(ProductModelID) INTO cont FROM ProductModel;
+:NEW.ProductModelID:=cont;
+END;
+
+
+CREATE OR REPLACE TRIGGER EL_MODELO
+BEFORE DELETE ON ProductModel
+FOR EACH ROW
+BEGIN
+IF :NEW.ProductModelID = 1
+THEN RAISE_APPLICATION_ERROR(-20000, 'NO SE PUEDE ELIMINAR');
+END IF;
+END;
