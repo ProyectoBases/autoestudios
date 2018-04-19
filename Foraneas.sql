0307f0c31f9df7a488b79b419e5a2e564f83a6a9
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-04-19 18:32:39 -0500

    Delete Foraneas.sql

--- Foraneas.sql
+++
@@ -1,6 +0,0 @@
---ProductCategory--
-ALTER TABLE ProductsCategories ADD CONSTRAINT FK_ProductCategory_PCategory FOREIGN KEY (parentProductCategoryID) REFERENCES ProductsCategories(productCategoryID);
-
---Product--
-ALTER TABLE Products ADD CONSTRAINT  FK_Product_ProductCategory FOREIGN KEY (productCategoryID) REFERENCES ProductsCategories(productCategoryID);
-ALTER TABLE Products ADD CONSTRAINT  FK_Product_ProductModel FOREIGN KEY (productModelID) REFERENCES ProductsModels(productModelID);
