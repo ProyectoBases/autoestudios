7fef44cdb48ed5c4e5d1bcb9dbd3d7a3883f91fa
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-04-19 18:32:29 -0500

    Delete Primarias.sql

--- Primarias.sql
+++
@@ -1,8 +0,0 @@
---ProductModel--
-ALTER TABLE ProductsModels ADD CONSTRAINT PK_PRODUCTMODEL PRIMARY KEY (productModelID);
-
---ProductCategory--
-ALTER TABLE ProductsCategories ADD CONSTRAINT PK_PRODUCTCATEGORY PRIMARY KEY (productCategoryID);
-
---Product--
-ALTER TABLE Products ADD CONSTRAINT  PK_PRODUCT PRIMARY KEY (productID);
