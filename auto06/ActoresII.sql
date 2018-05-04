f74f19579888c359d10f1ba29ffb284e1fb0d4e8
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-04 01:04:03 -0500

    Correccion ActoresII

--- auto06/ActoresII.sql
+++ auto06/ActoresII.sql
@@ -1,18 +1,39 @@
-CREATE OR REPLACE PACKAGE BODY MantenerCategoria AS
-    PROCEDURE ADCategoria (ID NUMBER, PARENT NUMBER, NOMBRE VARCHAR) AS
+CREATE OR REPLACE PACKAGE BODY PA_Administrador AS
+
+    FUNCTION  Consultar_Modelo(modelo VARCHAR) RETURN XMLTYPE IS Cons_Modelo XMLTYPE;
     BEGIN
-    INSERT INTO productCategory VALUES (ID,PARENT,NOMBRE);
-    END ADCategoria;
+        Cons_Modelo := PC_Mantener_Modelo.Consultar_Modelo(modelo);
+        RETURN Cons_Modelo;
+    END;
     
-    PROCEDURE ACCategoria (ID NUMBER, PARENT NUMBER, NOMBRE VARCHAR) AS
+    FUNCTION  Consultar_Info_Modelo RETURN SYS_REFCURSOR IS Info_Modelo SYS_REFCURSOR;
     BEGIN
-    UPDATE productCategory SET name = nombre
-    WHERE productCategoryID = ID AND parentProductCategory = PARENT;
-    END ACCategoria;
+        Info_Modelo := PC_Mantener_Modelo.Consultar_Info_Modelo;
+        RETURN Info_Modelo;
+    END;
     
-    PROCEDURE ELCategoria (ID NUMBER, PARENT NUMBER, NOMBRE VARCHAR) AS
-    BEGIN 
-    DELETE FROM productCategory WHERE productCategoryID = ID AND parentProductCategory = PARENT;
-    END ELCategoria;
+    FUNCTION  Consultar_Num_Productos(ciudad VARCHAR) RETURN SYS_REFCURSOR IS Num_Pro SYS_REFCURSOR;
+    BEGIN
+        Num_Pro := PC_Mantener_Categoria.Consultar_Num_Productos(ciudad);
+        RETURN Num_Pro;
+    END;    
     
-END MantenerCategoria;
+END PA_Administrador;
+
+/
+
+CREATE OR REPLACE PACKAGE BODY PA_Cliente AS    
+
+    FUNCTION  Consultar_Info_Modelo RETURN SYS_REFCURSOR IS Info_Modelo SYS_REFCURSOR;
+    BEGIN
+        Info_Modelo := PC_Mantener_Modelo.Consultar_Info_Modelo;
+        RETURN Info_Modelo;
+    END;
+    
+    FUNCTION  Consultar_Producto(pais VARCHAR) RETURN SYS_REFCURSOR IS Cons_Pro SYS_REFCURSOR;
+    BEGIN
+        Cons_Pro := PC_Mantener_Modelo.Consultar_Producto(pais);
+        RETURN Cons_Pro;
+    END;
+    
+END PA_Cliente;
